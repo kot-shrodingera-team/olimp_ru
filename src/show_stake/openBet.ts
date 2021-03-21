a49a16a5bd2581cd64d87b47cc570ff63ec6e462
kot-shrodingera <kot.shrodingera@gmail.com>
2021-03-21 11:02:07 +0600

    feat(openBet): оптимизация поиска ставки

--- src/show_stake/openBet.ts
+++ src/show_stake/openBet.ts
@@ -41,6 +41,7 @@ const openBet = async (): Promise<boolean> => {
   // ЦУПИС
   log(`Ищем ставку "${worker.BetName}"`, 'steelblue');
   await getElement('[class*="FullMatchStyled"] [class*="GroupOutcomes"]');
+  const reactKey = Number(worker.BetId.substring(0, worker.BetId.indexOf('_')));
 
   const outcome = await awaiter(
     () => {
@@ -49,33 +50,32 @@ const openBet = async (): Promise<boolean> => {
           '[class*="FullMatchStyled"] [class*="GroupOutcomes"]'
         ),
       ];
-      outcomeGroups.forEach((group) => {
-        if (!group.querySelector('ul')) {
-          group.querySelector('div').click();
-        }
-      });
-      const outcomeGroupLists = [
-        ...document.querySelectorAll(
-          '[class*="FullMatchStyled"] [class*="GroupOutcomes"] ul[class*="List"]'
-        ),
-      ];
-      let result = null;
-      const reactKey = Number(
-        worker.BetId.substring(0, worker.BetId.indexOf('_'))
-      );
+
       // eslint-disable-next-line no-restricted-syntax
-      for (const outcomeGroupList of outcomeGroupLists) {
-        // console.log(outcomeGroupList.previousElementSibling.textContent);
-        result = (getReactInstance(
-          outcomeGroupList
-        ) as OutcomeGroupList).memoizedProps.children.find((currentOutcome) => {
-          // console.log(
-          //   `${currentOutcome.props.children.props.name} - ${currentOutcome.props.children.props.id}`
-          // );
-          return currentOutcome.props.children.props.id === reactKey;
-        });
-        if (result) {
-          return result;
+      for (const outcomeGroup of outcomeGroups) {
+        if (!outcomeGroup.querySelector('ul')) {
+          outcomeGroup.querySelector('div').click();
+        }
+        const outcomeGroupLists = [
+          ...outcomeGroup.querySelectorAll('ul[class*="List"]'),
+        ];
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const outcomeGroupList of outcomeGroupLists) {
+          // console.log(outcomeGroupList.previousElementSibling.textContent);
+          const result = (getReactInstance(
+            outcomeGroupList
+          ) as OutcomeGroupList).memoizedProps.children.find(
+            (currentOutcome) => {
+              // console.log(
+              //   `${currentOutcome.props.children.props.name} - ${currentOutcome.props.children.props.id}`
+              // );
+              return currentOutcome.props.children.props.id === reactKey;
+            }
+          );
+          if (result) {
+            return result;
+          }
         }
       }
       return null;
